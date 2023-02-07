4e84a7c9195c3c446d6af16a32feb1e971bca85b
erinsawyer504 <erinsawyer504@gmail.com>
2023-02-07 14:28:40 -0500

    Added inquirer prompts to add manager

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-//TODO: include Employee, Manager, Engineer, Intern classes
-
-//TODO: Manager, Engineer, Intern extends Employee
+//linking team profiles
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 //TODO: not a requirement but add validation to ensure that user input is in the proper format
 
+//setting array to take in the entire team
+const wholeTeam = [];
 
+//setting up inquirer prompts to add manager
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the manager of the team.',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the manager's ID.",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the manager's email address.",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter the manager's office number",
+        },
+
+    ])
+    .then(managerData => {
+        const {name, id, email, officeNumber} = managerData;
+        const manager = new Manager (name, id, email, officeNumber);
+        wholeTeam.push(manager);
+        console.log(manager);
+    })
+};
+addManager();
+
+// {
+//     type: 'list',
+//     name: 'role',
+//     message: "Please select employee's role",
+//     choices: []
+// },
