1c6037675f310d84953eae3dd35839c2c1a74d8d
erinsawyer504 <erinsawyer504@gmail.com>
2023-02-07 11:05:17 -0500

    Added Employee class and todo list for assignments

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const inquirer = require("inquirer");
+
+//TODO: include Employee, Manager, Engineer, Intern classes
+
+//TODO: Manager, Engineer, Intern extends Employee
+
+//TODO: Manager will have officeNumber & getRole() overrides to return Manager
+
+//TODO: Engineer includes github property (github username), & include getGitHub(), & getRole() returns Engineer
+
+//TODO: Intern includes school property, getSchool(), and getRole() returns intern
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName(name) {
+    console.log('name: ', this.name);
+    }
+    getId(id) {
+    console.log('id: ', this.id);
+    }
+    getEmail(email) {
+    console.log('email: ', this.email);
+    }
+    //getRole() = returns 'Employee'
+}
+
+const alia = new Employee('Alia', '58157', 'Alia@cat.com');
+alia.getName();
+alia.getId();
+alia.getEmail();
+
